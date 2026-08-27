f3e35756f711c9b57113c08a4e822dc1aaa51b9c
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-08-27 17:32:08 +0200

    feat: add basic render function and Template for the Pokemon Cards

---
+++ js/template.js
@@ -0,0 +1,5 @@
+function getTemplatePokemonCard(index) {
+    return `<div class="pokemon-card">
+        <h1>${allPkm[index].name}</h1>
+            </div>`
+}
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let pokemon = "pokemon"
-
+let allPkm = [];
+let pokemon = "pokemon?limit=20&offset=0";
 
 function init() {
 loadPokemon(pokemon)    
@@ -11,5 +11,13 @@ async function loadPokemon(path="") {
     let response = await fetch(baseUrl +path)
     let responseToJson = await response.json();
     console.table(responseToJson.results);
+    allPkm.push(responseToJson.results);
 } 
 
+function renderPokemonCards() {
+    for (let index = 0; index < allPkm.length; index++) {
+        document.getElementById('pokemon-container').innerHTML += getTemplatePokemonCard();
+        
+        renderPokemonCards();
+    }
+}
