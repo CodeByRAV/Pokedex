b7df91b50e20f7782bd3a6ba1622ac0a346eb8c8
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-08-27 18:14:32 +0200

    fix: names and background of pokemon cards now displaying

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let allPkm = [];
 let pokemon = "pokemon?limit=20&offset=0";
 
-function init() {
-loadPokemon(pokemon)    
+async function init() {
+console.log("Init is Running")
+await loadPokemon(pokemon);
+renderPokemonCards();
 }
 
 const baseUrl = "https://pokeapi.co/api/v2/"
@@ -11,14 +13,13 @@ async function loadPokemon(path="") {
     let response = await fetch(baseUrl +path)
     let responseToJson = await response.json();
     console.table(responseToJson.results);
+    allPkm = responseToJson.results
+    console.log(allPkm)
 } 
 
 function renderPokemonCards() {
+    console.log("RENDERING", allPkm.length);
     for (let iPkm = 0; iPkm < allPkm.length; iPkm++) {
         document.getElementById('pokemon-container').innerHTML += getTemplatePokemonCard(iPkm);
-        
-
     }
-    
-    return renderPokemonCards();
 }
