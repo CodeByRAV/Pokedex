ce18feb472bde3ea6e5ebe3dffd283d17d2f3f81
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-08-31 17:01:22 +0200

    feat: Pokemon name's first letter are now capitalized

--- js/template.js
+++ js/template.js
@@ -1,7 +1,7 @@
-function getTemplatePokemonCard(iPkm) {
+function getTemplatePokemonCard(iPkm, pokeName) {
     return `
     <div class="pokemon-card ${allPkmDetails[iPkm].types[0].type.name}">
-        <div class="pokemon-name"><h1>${allPkm[iPkm].name}</h1></div>
+        <div class="pokemon-name"><h1>${pokeName}</h1></div>
         <img class="poke-img" src="${allPkmDetails[iPkm].sprites["front_default"]}"></img>
         <div class="type-icons"></div>
     </div>`
--- script.js
+++ script.js
@@ -15,18 +15,23 @@ async function loadPokemon(path="") {
     let responseToJson = await response.json();
     console.table(responseToJson.results);
     allPkm = responseToJson.results
-    console.log(allPkm)
+    console.log(allPkm);
 } 
+
+function formatPokemonName(name) {
+    let formattedName = name.charAt(0).toUpperCase() +
+    name.slice(1);
+    return formattedName;
+}
 
 async function renderPokemonCards() {
     console.log("RENDERING", allPkm.length);
     for (let iPkm = 0; iPkm < allPkm.length; iPkm++) {
+        let pokeName = formatPokemonName(allPkm[iPkm].name);
         let response = await fetch(allPkm[iPkm].url)
         let responseToJson = await response.json();
-        console.log(responseToJson)
         allPkmDetails.push(responseToJson);
-        document.getElementById('pokemon-container').innerHTML += getTemplatePokemonCard(iPkm);
-        
+        document.getElementById('pokemon-container').innerHTML += getTemplatePokemonCard(iPkm, pokeName);
     }
     console.log(allPkmDetails);
 }
