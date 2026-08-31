6bebc463175dfc0f08e28c47b63da2a2702fa764
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-08-31 17:24:55 +0200

    feat: add structure for Pokemon type icons

--- js/template.js
+++ js/template.js
@@ -3,7 +3,17 @@ function getTemplatePokemonCard(iPkm, pokeName) {
     <div class="pokemon-card ${allPkmDetails[iPkm].types[0].type.name}">
         <div class="pokemon-name"><h1>${pokeName}</h1></div>
         <img class="poke-img" src="${allPkmDetails[iPkm].sprites["front_default"]}"></img>
-        <div class="type-icons"></div>
+        <div class="type-icons">
+        ${getTemplateTypeIcons(iPkm)}</div>
     </div>`
 }
 
+function getTemplateTypeIcons(iPkm) {
+    let typeIcons = "";
+
+    for (let iType = 0; iType < allPkmDetails[iPkm].types.length; iType++) {
+        let pokeTypeIcon = allPkmDetails[iPkm].types[iType].type.name
+   typeIcons += `
+    <img src="${pokeTypeIcon}">
+    `} return typeIcons; 
+}
