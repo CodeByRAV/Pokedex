5a306bd39523f7bc8b73af343ea98b244343b721
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-08-31 15:24:45 +0200

    feat: display pokemon names and images

--- js/template.js
+++ js/template.js
@@ -1,5 +1,6 @@
 function getTemplatePokemonCard(iPkm) {
     return `<div class="pokemon-card">
         <h1>${allPkm[iPkm].name}</h1>
+        <img class="poke-img" src="${allPkmDetails[iPkm].sprites["front_default"]}"></img>
             </div>`
 }
--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 let allPkm = [];
+let allPkmDetails =[];
 let pokemon = "pokemon?limit=20&offset=0";
 
 async function init() {
 console.log("Init is Running")
 await loadPokemon(pokemon);
-renderPokemonCards();
+await renderPokemonCards();
 }
 
 const baseUrl = "https://pokeapi.co/api/v2/"
 
 async function loadPokemon(path="") {
-    let response = await fetch(baseUrl +path)
+    let response = await fetch(baseUrl + path)
     let responseToJson = await response.json();
     console.table(responseToJson.results);
     allPkm = responseToJson.results
     console.log(allPkm)
 } 
 
-function renderPokemonCards() {
+async function renderPokemonCards() {
     console.log("RENDERING", allPkm.length);
     for (let iPkm = 0; iPkm < allPkm.length; iPkm++) {
+        let response = await fetch(allPkm[iPkm].url)
+        let responseToJson = await response.json();
+        console.log(responseToJson)
+        allPkmDetails.push(responseToJson);
         document.getElementById('pokemon-container').innerHTML += getTemplatePokemonCard(iPkm);
+        console.log(allPkmDetails);
     }
 }
